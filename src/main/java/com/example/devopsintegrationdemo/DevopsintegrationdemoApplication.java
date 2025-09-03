a7ec5234a714d7304b8bcbdec7bd899852590782
lilly0709 <fousia2705@gmail.com>
2025-09-03 12:05:47 -0700

    first commit

---
+++ src/main/java/com/example/devopsintegrationdemo/DevopsintegrationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsintegrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsintegrationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsintegrationdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopsintegrationdemo/DevopsintegrationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsintegrationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsintegrationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
